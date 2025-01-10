8191a833b6720d3527b2349df2c7abc4f18ef237
Evgenii Govorushkin <evgenii.govorushkin@telekom.com>
2025-01-10 22:34:03 +0200

    Initial commit

---
+++ src/test/java/com/jobgem/JobgemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobgem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobgemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

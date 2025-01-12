51bb2dd7ff344f84fe01a49a839ab3b05ebc4647
Evgenii Govorushkin <evgenii.govorushkin@telekom.com>
2025-01-12 22:27:53 +0200

    added Exception handler

--- src/main/java/com/jobgem/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jobgem/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.jobgem.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        logger.error(ex.getMessage(), ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }

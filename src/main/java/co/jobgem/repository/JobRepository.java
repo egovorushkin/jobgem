ed94e0f33205050bd562e4f112271b33c646b45c
Evgenii Govorushkin <e.govorushkin@gmail.com>
2025-02-07 21:29:47 +0200

    feat: add repository

---
+++ src/main/java/co/jobgem/repository/JobRepository.java
@@ -0,0 +1,9 @@
+package co.jobgem.repository;
+
+import co.jobgem.entity.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long> {
+}

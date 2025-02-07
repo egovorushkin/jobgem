b318c950d211753b888d32efa10cb3cfd41d116a
Evgenii Govorushkin <e.govorushkin@gmail.com>
2025-02-07 21:31:45 +0200

    feat: add Skill repository

---
+++ src/main/java/co/jobgem/repository/SkillRepository.java
@@ -0,0 +1,9 @@
+package co.jobgem.repository;
+
+import co.jobgem.entity.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+}
